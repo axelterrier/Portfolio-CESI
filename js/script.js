0babd53e9f39453417005b165b58f409ed1a9565
Axel TERRIER <47625927+axelterrier@users.noreply.github.com>
2022-06-07 00:09:10 +0200

    V2

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+function scrollToHome(){
+    document.getElementById('home').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function scrollToPortfolio() {
+    document.getElementById('portfolio').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+function scrollToResume() {
+    document.getElementById('resume').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
